ae8af04c8b80f25df30786d19e764dcd6f96171c
DESKTOP-DTVTGOG\imnew <imnewinthecity@nate.com>
2022-01-02 11:37:31 +0900

    20220102

---
+++ src/com/cafe24Test/Test2.java
@@ -0,0 +1,19 @@
+package com.cafe24Test;
+
+import java.util.Scanner;
+
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in); 
+		
+		int N = Integer.parseInt(sc.nextLine());
+		String str = sc.nextLine();
+
+		System.out.print("s");
+		
+	}
+
+}
